f2e6f637b167aa125918959f73fa8895ba313a23
Olga Ivanova <olich_ivanova@mail.ru>
2020-11-23 12:42:22 +0300

    initial commit

--- src/by/belhard26/homework/homework2/task1.java
+++ src/by/belhard26/homework/homework2/task1.java
@@ -1,14 +1,20 @@
 package by.belhard26.homework.homework2;
 
+import java.util.Arrays;
+
 public class task1 {
   public static void main(String[] args) {
-        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int array[] = {0, 0, 11, 0, 0, 100};
         int sum = 0;
+        int x;
         double median;
         for (int i :array) sum += i;
         System.out.println("Summa = " + sum);
         double aver = sum / array.length;
         System.out.println("Average  value = " + aver);
+      Arrays.sort(array);
+      System.out.println("After sorting: " + Arrays.toString(array) );
+      System.out.println();
         if (array.length % 2 == 0) {
             median = array[(array.length-1)/2] + array[array.length/2];
             System.out.println(median / 2);
